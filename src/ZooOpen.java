37bd06aae6aac6feb97529baf1ada96ef07f0112
PLara-r <tl1226932@gmail.com>
2020-08-02 16:09:33 +0300

    Initial commit

---
+++ src/ZooOpen.java
@@ -0,0 +1,37 @@
+
+
+import java.util.*;
+
+public class ZooOpen {
+
+               public static void main(String[] args) {
+                            Locale us = new Locale("en", "US");
+                             Locale france = new Locale("fr", "FR");
+
+                             printProperties(us);
+                             System.out.println();
+                            printProperties(france);
+
+                   Locale us1 = new Locale("en", "US");
+                   ResourceBundle rb = ResourceBundle.getBundle("Zoo", us1);
+                   Set<String> keys = rb.keySet();
+                   keys.stream().map(k -> k + " " + rb.getString(k))
+                           .forEach(System.out::println);
+
+                   Properties props = new Properties();
+                   rb.keySet().stream()
+                           .forEach(k -> props.put(k, rb.getString(k)));
+
+                   System.out.println(props.getProperty("h"));
+                   System.out.println(props.getProperty("h", "123"));
+
+
+
+
+               }
+            public static void printProperties(Locale locale) {
+                ResourceBundle rb = ResourceBundle.getBundle("Zoo", locale);
+                System.out.println(rb.getString("hello"));
+                System.out.println(rb.getString("open"));
+             }
+  }
